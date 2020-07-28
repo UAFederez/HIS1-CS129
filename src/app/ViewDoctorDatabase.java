d4da982cfc83548d33edf12a2a20a6312503cf12
uafederez <uafederez@gmail.com>
2020-07-28 09:14:29 +0800

    added list doctors and with rates

--- src/app/ViewDoctorDatabase.java
+++ src/app/ViewDoctorDatabase.java
@@ -41,7 +41,11 @@ public class ViewDoctorDatabase extends javax.swing.JFrame {
 
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        listDoctors = new javax.swing.JTable();
         jPanel4 = new javax.swing.JPanel();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        listRateTable = new javax.swing.JTable();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         specializationTable = new javax.swing.JTable();
@@ -51,28 +55,64 @@ public class ViewDoctorDatabase extends javax.swing.JFrame {
 
         setTitle("View Doctor Database");
 
+        listDoctors.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "First Name", "Last Name", "Specialization"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane3.setViewportView(listDoctors);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 615, Short.MAX_VALUE)
+            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 615, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 497, Short.MAX_VALUE)
+            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 497, Short.MAX_VALUE)
         );
 
         jTabbedPane1.addTab("List Doctors", jPanel1);
+
+        listRateTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "First Name", "Last Name", "Specialization", "Consultation Rate"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane4.setViewportView(listRateTable);
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 615, Short.MAX_VALUE)
+            .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 615, Short.MAX_VALUE)
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 497, Short.MAX_VALUE)
+            .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 497, Short.MAX_VALUE)
         );
 
         jTabbedPane1.addTab("List wth Consultation Rates", jPanel4);
@@ -174,10 +214,41 @@ public class ViewDoctorDatabase extends javax.swing.JFrame {
     
     public void refreshTables()
     {
+        updateListDoctorsTable();
+        updateConsultRateTable();
         updateSpecializationsTable();
         updateSortedSchedTable();
     }
     
+    private void updateListDoctorsTable() {
+        DefaultTableModel model = (DefaultTableModel) listDoctors.getModel();
+        model.setRowCount(0);
+        
+        for(Doctor d : doctors)
+        {   
+            String dfname  = d.getFirstName();
+            String dlname  = d.getLastName();
+            String special = d.getSpecialization();
+            
+            model = (DefaultTableModel) listDoctors.getModel();
+            model.insertRow(model.getRowCount(), new Object[]{dfname, dlname, special});
+        }
+        
+    }
+    
+    private void updateConsultRateTable() {
+        DefaultTableModel model = (DefaultTableModel) listRateTable.getModel();
+        model.setRowCount(0);
+        
+        for(Doctor d : doctors)
+        {
+            model.insertRow(model.getRowCount(), new Object[]{d.getFirstName(), 
+                                                              d.getLastName(),
+                                                              d.getSpecialization(),
+                                                              "PHP " + d.getConsultationRate()});
+        }
+    }
+        
     private void updateSpecializationsTable()
     {
         Collections.sort(doctors, new Comparator<Doctor>() {
@@ -301,7 +372,11 @@ public class ViewDoctorDatabase extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JTable listDoctors;
+    private javax.swing.JTable listRateTable;
     private javax.swing.JTable sortedSchedTable;
     private javax.swing.JTable specializationTable;
     // End of variables declaration//GEN-END:variables
